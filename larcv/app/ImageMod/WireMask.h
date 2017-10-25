ddc7427eb8c27e9e411687f0054b723f11b84425
Gustavo Valdiviesso <gustavogx@gmail.com>
2017-10-25 16:03:42 -0500

    WireMask.cxx modified to accomodate new IOManager::get_data, EventImage2D and EventChStatus access members.

--- larcv/app/ImageMod/WireMask.h
+++ larcv/app/ImageMod/WireMask.h
@@ -14,8 +14,8 @@
 #ifndef __WIREMASK_H__
 #define __WIREMASK_H__
 
-#include "Processor/ProcessBase.h"
-#include "Processor/ProcessFactory.h"
+#include "larcv/core/Processor/ProcessBase.h"
+#include "larcv/core/Processor/ProcessFactory.h"
 namespace larcv {
 
   /**
