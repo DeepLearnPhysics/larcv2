afce0c0d5f7c2daa5f72f0c4bea00eb9fc16e9f1
Gustavo Valdiviesso <gustavogx@gmail.com>
2017-10-25 16:39:45 -0500

    Modification to CombineImages.cxx

--- larcv/app/ImageMod/CombineImages.h
+++ larcv/app/ImageMod/CombineImages.h
@@ -14,8 +14,8 @@
 #ifndef __COMBINEIMAGES_H__
 #define __COMBINEIMAGES_H__
 
-#include "Processor/ProcessBase.h"
-#include "Processor/ProcessFactory.h"
+#include "larcv/core/Processor/ProcessBase.h"
+#include "larcv/core/Processor/ProcessFactory.h"
 namespace larcv {
 
   /**
