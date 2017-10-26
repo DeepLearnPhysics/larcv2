41ac2684986f163474f418f0da79ffc4f6044524
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-10-25 19:19:10 -0500

    1 more utility emplace

--- larcv/core/DataFormat/Voxel.h
+++ larcv/core/DataFormat/Voxel.h
@@ -194,7 +194,7 @@ namespace larcv {
 
     void set(const larcv::VoxelSetArray& voxel_vv, const larcv::ImageMeta& meta);
 
-    void move(larcv::VoxelSetArray2D&& target)
+    void emplace(larcv::VoxelSetArray2D&& target)
     { _voxel_vvv = std::move(target._voxel_vvv); _meta_v = std::move(target._meta_v); }
 
   private:
