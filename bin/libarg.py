a24f045842ff66014f16ad113f4fc3858cc45e70
coreyjadams <coreyadams@fas.harvard.edu>
2017-12-04 11:12:25 -0600

    Changes to python commands for python3 - ought to be backwards compatible to python2.7

--- bin/libarg.py
+++ bin/libarg.py
@@ -1,12 +1,12 @@
 
-import sys,os,commands
+import sys,os,subprocess
 
 dirs=[]
 for d in os.listdir(os.environ['LARCV_BUILDDIR']):
     if not len([x for x in os.listdir('%s/%s' % (os.environ['LARCV_BUILDDIR'],d)) if x.endswith('.o')]): continue
     dirs.append(d)
-libs=[x for x in commands.getoutput('larcv-config --libs').split() if not x.startswith('-llarcv')]
-libs+= commands.getoutput('root-config --libs').split()
+libs=[x for x in subprocess.getoutput('larcv-config --libs').split() if not x.startswith('-llarcv')]
+libs+= subprocess.getoutput('root-config --libs').split()
 if 'PYTHON_LIB' in os.environ:
     libs+= [" -L%s -lpython2.7"%(os.environ["PYTHON_LIB"].strip())]
 
@@ -51,6 +51,6 @@ for d in dict_list:
 for l in libs:
     cmd += '%s ' % l
 
-if 'build' in sys.argv: print 1
-else: print cmd
+if 'build' in sys.argv: print(1)
+else: print(cmd)
 sys.exit(1)
