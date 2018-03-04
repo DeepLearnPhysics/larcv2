1d767aec92238e42ef674178400b951d41d20f02
drinkingkazu <kazuhiro@nevis.columbia.edu>
2018-03-04 02:19:31 -0800

    support all types

--- larcv/core/PyUtil/PyUtils.cxx
+++ larcv/core/PyUtil/PyUtils.cxx
@@ -17,23 +17,6 @@ void SetPyUtil() {
   }
 }
 
-PyObject *as_ndarray(const std::vector<float> &vec) {
-  SetPyUtil();
-
-  if (vec.size() >= INT_MAX) {
-    LARCV_CRITICAL() << "Length of data vector too long to specify ndarray. "
-                        "Use by batch call."
-                     << std::endl;
-    throw larbys();
-  }
-  int nd = 1;
-  npy_intp dims[1];
-  dims[0] = (int)vec.size();
-  PyArrayObject *array = (PyArrayObject *)PyArray_SimpleNewFromData(
-      nd, dims, NPY_FLOAT, (char *)&(vec[0]));
-  return PyArray_Return(array);
-}
-
 PyObject *as_ndarray(const Image2D &img) {
   SetPyUtil();
   npy_intp dim_data[2];
@@ -44,14 +27,14 @@ PyObject *as_ndarray(const Image2D &img) {
   //return PyArray_FromDimsAndData(2, dim_data, NPY_FLOAT, (char *)&(vec[0]));
 }
 
+/*
 void copy_array(PyObject *arrayin, const std::vector<float> &cvec) {
   SetPyUtil();
   PyArrayObject *ptr = (PyArrayObject *)(arrayin);
-  /*
-  std::cout<< PyArray_NDIM(ptr) << std::endl
-           << PyArray_DIM(ptr,0)<<std::endl
-           << PyArray_SIZE(ptr) << std::endl;
-  */
+
+  //std::cout<< PyArray_NDIM(ptr) << std::endl
+  //         << PyArray_DIM(ptr,0)<<std::endl
+  //         << PyArray_SIZE(ptr) << std::endl;
 
   // Check dimension size is 1:
   if (PyArray_NDIM(ptr) != 1){
@@ -63,6 +46,32 @@ void copy_array(PyObject *arrayin, const std::vector<float> &cvec) {
   npy_intp loc[1];
   loc[0] = 0;
   auto fptr = (float *)(PyArray_GetPtr(ptr, loc));
+  for (size_t i = 0; i < size_t(PyArray_SIZE(ptr)); ++i) {
+    // std::cout << fptr[i] << std::endl;
+    fptr[i] = cvec[i];
+  };
+}
+*/
+
+template<class T>
+void copy_array(PyObject *arrayin, const std::vector<T> &cvec) {
+  SetPyUtil();
+  PyArrayObject *ptr = (PyArrayObject *)(arrayin);
+
+  //std::cout<< PyArray_NDIM(ptr) << std::endl
+  //         << PyArray_DIM(ptr,0)<<std::endl
+  //         << PyArray_SIZE(ptr) << std::endl;
+
+  // Check dimension size is 1:
+  if (PyArray_NDIM(ptr) != 1){
+    throw std::exception();
+  }
+
+  if ((long)(cvec.size()) != PyArray_SIZE(ptr))
+    throw std::exception();
+  npy_intp loc[1];
+  loc[0] = 0;
+  auto fptr = (T *)(PyArray_GetPtr(ptr, loc));
   for (size_t i = 0; i < size_t(PyArray_SIZE(ptr)); ++i) {
     // std::cout << fptr[i] << std::endl;
     fptr[i] = cvec[i];
@@ -177,6 +186,106 @@ void fill_img_col(Image2D &img, std::vector<short> &adcs, const int col, const f
     img.set_pixel(irow, col, val + ((float)adcs.at(iadc) - pedestal));
   }
 }
+
+template<> int ctype_to_numpy<short>() { SetPyUtil(); SetPyUtil(); return NPY_INT16; }
+template<> int ctype_to_numpy<unsigned short>() { SetPyUtil(); return NPY_INT16; }
+template<> int ctype_to_numpy<int>() { SetPyUtil(); return NPY_INT32; }
+template<> int ctype_to_numpy<unsigned int>() { SetPyUtil(); return NPY_UINT32; }
+template<> int ctype_to_numpy<long long>() { SetPyUtil(); return NPY_INT64; }
+template<> int ctype_to_numpy<unsigned long long>() { SetPyUtil(); return NPY_UINT64; }
+template<> int ctype_to_numpy<float>() { SetPyUtil(); return NPY_FLOAT32; }
+template<> int ctype_to_numpy<double>() { SetPyUtil(); return NPY_FLOAT64; }
+
+/*
+PyObject *as_ndarray(const std::vector<float> &vec) {
+  SetPyUtil();
+
+  if (vec.size() >= INT_MAX) {
+    LARCV_CRITICAL() << "Length of data vector too long to specify ndarray. "
+                        "Use by batch call."
+                     << std::endl;
+    throw larbys();
+  }
+  int nd = 1;
+  npy_intp dims[1];
+  dims[0] = (int)vec.size();
+  PyArrayObject *array = (PyArrayObject *)PyArray_SimpleNewFromData(
+      nd, dims, NPY_FLOAT, (char *)&(vec[0]));
+  return PyArray_Return(array);
+}
+*/
+
+template <class T>
+PyObject *as_ndarray(const std::vector<T> &vec) {
+  SetPyUtil();
+
+  if (vec.size() >= INT_MAX) {
+    LARCV_CRITICAL() << "Length of data vector too long to specify ndarray. "
+                        "Use by batch call."
+                     << std::endl;
+    throw larbys();
+  }
+  int nd = 1;
+  npy_intp dims[1];
+  dims[0] = (int)vec.size();
+  PyArrayObject *array = (PyArrayObject *)PyArray_SimpleNewFromData(
+      nd, dims, ctype_to_numpy<T>(), (char *)&(vec[0]));
+  return PyArray_Return(array);
+}
+
+template PyObject* as_ndarray< short              > (const std::vector< short              >& vec);
+template PyObject* as_ndarray< unsigned short     > (const std::vector< unsigned short     >& vec);
+template PyObject* as_ndarray< int                > (const std::vector< int                >& vec);
+template PyObject* as_ndarray< unsigned int       > (const std::vector< unsigned int       >& vec);
+template PyObject* as_ndarray< long long          > (const std::vector< long long          >& vec);
+template PyObject* as_ndarray< unsigned long long > (const std::vector< unsigned long long >& vec);
+template PyObject* as_ndarray< float              > (const std::vector< float              >& vec);
+template PyObject* as_ndarray< double             > (const std::vector< double             >& vec);
+
+template<class T>
+PyObject* numpy_array(std::vector<size_t> dims)
+{
+  SetPyUtil();
+  int nd_ = dims.size();
+  npy_intp dims_[nd_];
+  for(size_t i=0; i<dims.size(); ++i) dims_[i] = dims[i];
+  /*
+  std::cout<<"NUMPY TYPE " << ctype_to_numpy<T>() << std::endl;
+  std::cout<<"DIMS " << dims.size() << std::endl;
+  std::cout<<"ND " << nd_ << std::endl;
+  std::cout<<"Shape ";
+  for(size_t i=0; i<dims.size(); ++i) std::cout<< " " << dims_[i];
+  std::cout<<std::endl;
+  */
+  //PyObject* res = PyArray_SimpleNew(nd_,dims_,ctype_to_numpy<T>());
+  PyObject* res = PyArray_ZEROS(nd_,dims_,ctype_to_numpy<T>(),0);
+  //Py_INCREF(res);
+  /*
+  std::cout<<PyArray_NDIM((PyArrayObject*)res) << std::endl;
+  std::cout<<PyArray_SIZE((PyArrayObject*)res) << std::endl;
+  */
+  PyArrayObject *ptr = (PyArrayObject*)(res);
+  // Check dimension size is 1:
+  //std::cout<<"ndim " << PyArray_NDIM(ptr) << std::endl;
+  size_t len = PyArray_SIZE(ptr);
+  //std::cout<<"len " << len <<std::endl; 
+  npy_intp loc[1];
+  loc[0] = 0;
+  auto fptr = (T *)(PyArray_GetPtr(ptr, loc));
+  /*
+  std::cout<<"fptr " << fptr << std::endl;
+  for (size_t i = 0; i < len; ++i) {
+    std::cout << fptr[i] << std::endl;
+    fptr[i] = T(1);
+  }
+  */
+  PyArray_INCREF(ptr);
+
+  return res;
+}
+
+template PyObject* numpy_array<float>(std::vector<size_t>dims);
+
 }
 
 #endif
