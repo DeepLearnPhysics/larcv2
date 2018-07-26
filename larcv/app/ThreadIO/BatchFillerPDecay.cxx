a1f763849cd912620614e7e91374ce757d538e09
drinkingkazu <kazuhiro@nevis.columbia.edu>
2018-07-26 07:14:17 -0500

    new classification logic

--- larcv/app/ThreadIO/BatchFillerPDecay.cxx
+++ larcv/app/ThreadIO/BatchFillerPDecay.cxx
@@ -17,7 +17,7 @@ namespace larcv {
   void BatchFillerPDecay::configure(const PSet& cfg)
   {
     _part_producer = cfg.get<std::string>("ParticleProducer");
-    _num_class = 2;
+    _num_class = 5;
   }
 
   void BatchFillerPDecay::initialize()
@@ -75,18 +75,68 @@ namespace larcv {
       throw larbys();
     }
     */
-    // class
+
     size_t label = kINVALID_SIZE;
-    int pdg = 0;
+    // Check if this is not kaon decay
     for (auto const& part : part_v) {
-      pdg = part.pdg_code();
-      if(pdg == 321)
-	label = 0;
-      else
-	label = 1;
-      if (label != kINVALID_SIZE) break;
+      if(part.parent_track_id() != part.track_id()) continue;
+      if(part.pdg_code() != 321) label = 0;
+      break;
     }
-    LARCV_DEBUG() << "Found PDG code " << pdg << " (class=" << label << ")" << std::endl;
+    if(label == kINVALID_SIZE) {
+      // Now categorize into 3 cases
+      // 1) mu+ ... mono-energetic muon ... 63.55%
+      // 2) pi/mu + pi0 (+pi0) ... track + shower ... 20.66 + 3.353 + 1.761 = 25.774%
+      // 3) pi + pi + pi ... three tracks ... 5.59%
+      // 4) e + pi0 ... three shower ... 5.07%
+      int positron_ctr = 0;
+      int pion_ctr = 0;
+      int piminus_ctr = 0;
+      int neutral_pion_ctr = 0;
+      int muon_ctr = 0;
+      // First, count neutral pions by counting decay gamma
+      int gamma_ctr = 0;
+      for(auto const& part : part_v) {
+	if(part.parent_pdg_code() != 111) continue;
+	if(part.pdg_code() != 22) continue;
+	++gamma_ctr;
+      }
+      if(gamma_ctr > 2) neutral_pion_ctr = 2;
+      else if(gamma_ctr > 0) neutral_pion_ctr = 1;
+      // Second, count others
+      for(auto const& part : part_v) {
+	if(part.parent_pdg_code() != 321) continue;
+	if(part.track_id() == part.parent_track_id()) continue;
+	if(part.pdg_code() == -11) ++positron_ctr;
+	else if(part.pdg_code() == -13) ++muon_ctr;
+	else if(abs(part.pdg_code()) == 211) ++pion_ctr;
+	if(part.pdg_code() == -211) ++piminus_ctr;
+      }
+      // Assign label
+      if      (positron_ctr == 0 && pion_ctr == 0 && muon_ctr == 1 && neutral_pion_ctr == 0)
+	label = 1;
+      else if (positron_ctr == 0 && pion_ctr == 1 && muon_ctr == 0 && neutral_pion_ctr >  0)
+	label = 2;
+      else if (positron_ctr == 0 && pion_ctr == 0 && muon_ctr == 1 && neutral_pion_ctr >  0)
+	label = 2;
+      else if (positron_ctr == 0 && pion_ctr == 3 && muon_ctr == 0 && neutral_pion_ctr == 0)
+	label = 3;
+      else if (positron_ctr == 1 && pion_ctr == 0 && muon_ctr == 0 && neutral_pion_ctr >  0)
+	label = 4;
+
+      // In case some particles are missing, key only on unique particle count
+      if(label == kINVALID_SIZE) {
+	LARCV_WARNING() << "Keying on partial particle info (should be rare)..." << std::endl;
+	if      (positron_ctr == 1) label = 4;
+	else if (neutral_pion_ctr > 0) label = 2;
+	else if (piminus_ctr > 0) label = 3;
+      }
+      if(label == kINVALID_SIZE) {
+	LARCV_CRITICAL() << "Cannot classify this event! " << event_part.event_key() << std::endl;
+	throw larbys();
+      }
+    }
+
     _entry_data.resize(_num_class, 0);
     for (auto& v : _entry_data) v = 0;
     _entry_data.at(label) = 1.;
